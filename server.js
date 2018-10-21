c8f0b023679623731411269637deb6504c367d13
Recep Yaşar <ryasar79@gmail.com>
2018-10-21 16:24:50 +0300

    repository init edildi

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+
+var middleware = {
+    requireAuthentication: function (req, res, next) {
+        console.log('özel route girildi');
+        next();
+    },
+    logger: function (req, res, next) {
+        console.log(req.method + ' ' + req.originalUrl);
+        next();
+    }
+}
+
+app.use(middleware.logger);
+
+app.get('/hakkimda', middleware.requireAuthentication, function (req, res) {
+    res.send('Hakkımızda sayfası')
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+    console.log('Express server başlatıldı');
+
+});
